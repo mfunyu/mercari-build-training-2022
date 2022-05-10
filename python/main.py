dd4ae5cc606bbdbd1cfb39a1400159cc765cafc0
Yuu Funyu <mfunyu@student.42tokyo.jp>
2022-05-10 22:16:05 +0900

    feat: 2 List a new item

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pathlib
+import json
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,8 +24,17 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
+def add_item(name: str = Form(...), category: str = Form(...)):
     logger.info(f"Receive item: {name}")
+    
+    with open('item.json', 'a+') as f:
+        try:
+            data = json.load(f)
+        except:
+            data = {"items": []}
+        data["items"].append({"name": name, "category": category})
+        f.write(json.dumps(data))
+    
     return {"message": f"item received: {name}"}
 
 @app.get("/image/{items_image}")
